ca3c693e47fe21278db67050ba553ce410633691
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2019-04-29 04:23:33 +0000

    Installed @material-ui/core @material-ui/icons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import Icon from "@material-ui/core/Icon";
+import DeleteIcon from "@material-ui/icons/Delete";
+import NavigationIcon from "@material-ui/icons/Navigation";
 
-function App() {
+const styles = theme => ({
+  fab: {
+    margin: theme.spacing.unit
+  },
+  extendedIcon: {
+    marginRight: theme.spacing.unit
+  }
+});
+
+function App(props) {
+  const { classes } = props;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Fab color="primary" aria-label="Add" className={classes.fab}>
+        <AddIcon />
+      </Fab>
+      <Fab color="secondary" aria-label="Edit" className={classes.fab}>
+        <Icon>edit_icon</Icon>
+      </Fab>
+      <Fab variant="extended" aria-label="Delete" className={classes.fab}>
+        <NavigationIcon className={classes.extendedIcon} />
+        Extended
+      </Fab>
+      <Fab disabled aria-label="Delete" className={classes.fab}>
+        <DeleteIcon />
+      </Fab>
     </div>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
