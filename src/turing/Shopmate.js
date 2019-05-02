aade4be74eb8d5331bede40f46dd482d4d723df5
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2019-05-02 19:24:19 +0000

    Make body background as Paper

--- src/turing/Shopmate.js
+++ src/turing/Shopmate.js
@@ -3,7 +3,7 @@ import Logobar from "./Logobar";
 import Categories from "./Categories";
 import CategoriesPagination from "./CategoriesPagination";
 import CategoryItems from "./CategoryItems";
-import { Grid } from "@material-ui/core";
+import { Grid, Paper } from "@material-ui/core";
 
 const data = {
   searchAppBar: {
@@ -18,15 +18,17 @@ function Shopmate() {
   return (
     <div>
       <Logobar appbar={data.searchAppBar} />
-      <Grid container>
-        <Grid item sm={3}>
-          <Categories />
+      <Paper>
+        <Grid container>
+          <Grid item sm={3}>
+            <Categories />
+          </Grid>
+          <Grid item sm={9} container direction="column" alignItems="center">
+            <CategoriesPagination />
+            <CategoryItems />
+          </Grid>
         </Grid>
-        <Grid item sm={9} container direction="column" alignItems="center">
-          <CategoriesPagination />
-          <CategoryItems />
-        </Grid>
-      </Grid>
+      </Paper>
     </div>
   );
 }
