c2cc0fcf094cd1dff5330d8d9b93b3d7f978ac13
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2019-05-02 15:52:41 +0000

    Added flat-pagination implementation of CategoriesPagination

--- src/turing/CategoriesPagination.js
+++ src/turing/CategoriesPagination.js
@@ -1,7 +1,12 @@
 import React from "react";
+import ExampleFromNpmjs from "./material-ui-flat-pagination/ExampleFromNpmjs";
 
 function CategoriesPagination() {
-  return <div>CategoriesPagination</div>;
+  return (
+    <div>
+      <ExampleFromNpmjs />
+    </div>
+  );
 }
 
 export default CategoriesPagination;
