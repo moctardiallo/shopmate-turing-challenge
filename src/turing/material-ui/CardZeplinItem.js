89cb0c278fe0a73a7c7a8ed0a404114cdcf11555
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2019-05-02 18:04:57 +0000

    CardZeplinItem: added CardHeader and CardContent but image not showing

--- src/turing/material-ui/CardZeplinItem.js
+++ src/turing/material-ui/CardZeplinItem.js
@@ -1,7 +1,29 @@
 import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  Typography
+} from "@material-ui/core";
+import Bag from "./images-bag.png";
 
 function CardZeplinItem() {
-  return <div>CardZeplinItem</div>;
+  return (
+    <div>
+      <Card>
+        <CardHeader title="CardZeplinItem" />
+        <CardMedia image={Bag} />
+        <CardContent>
+          <Typography component="p">
+            This impressive paella is a perfect party dish and a fun meal to
+            cook together with your guests. Add 1 cup of frozen peas along with
+            the mussels, if you like.
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
 
 export default CardZeplinItem;
