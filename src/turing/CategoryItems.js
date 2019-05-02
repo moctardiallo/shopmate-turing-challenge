77637fecd354c60940a601d5b2a7696b83678e35
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2019-05-02 18:59:10 +0000

    Added customized data to category items

--- src/turing/CategoryItems.js
+++ src/turing/CategoryItems.js
@@ -1,7 +1,23 @@
 import React from "react";
 import MediaCard from "./material-ui/MediaCard";
 import { Grid, withStyles } from "@material-ui/core";
-import ImagesBag from "./turing-uiux/Zeplin/Images/images-bag.png";
+import ImagesShirt1 from "./turing-uiux/Zeplin/Images/images-shirt1.png";
+import ImagesShirt2 from "./turing-uiux/Zeplin/Images/images-shirt2.png";
+import ImagesShirt3 from "./turing-uiux/Zeplin/Images/images-shirt3.png";
+import ImagesShirt4 from "./turing-uiux/Zeplin/Images/images-shirt4.png";
+import ImagesShirt5 from "./turing-uiux/Zeplin/Images/images-shirt5.png";
+import ImagesShirt6 from "./turing-uiux/Zeplin/Images/images-shirt6.png";
+import ImagesShirt7 from "./turing-uiux/Zeplin/Images/images-shirt7.png";
+import ImagesShirt8 from "./turing-uiux/Zeplin/Images/images-shirt8.png";
+import ImagesShirt9 from "./turing-uiux/Zeplin/Images/images-shirt9.png";
+import ImagesShirt10 from "./turing-uiux/Zeplin/Images/images-shirt10.png";
+import ImagesShirt11 from "./turing-uiux/Zeplin/Images/images-shirt11.png";
+import ImagesShirt12 from "./turing-uiux/Zeplin/Images/images-shirt12.png";
+import ImagesShirt13 from "./turing-uiux/Zeplin/Images/images-shirt13.png";
+import ImagesShirt14 from "./turing-uiux/Zeplin/Images/images-shirt14.png";
+import ImagesShirt15 from "./turing-uiux/Zeplin/Images/images-shirt15.png";
+import ImagesShirt16 from "./turing-uiux/Zeplin/Images/images-shirt16.png";
+import ImagesShirt17 from "./turing-uiux/Zeplin/Images/images-shirt17.png";
 
 const styles = {
   root: {
@@ -12,23 +28,88 @@ const styles = {
 
 const items = [
   {
-    title: "Bag",
-    image: ImagesBag,
+    title: "Arc Triomphe",
+    image: ImagesShirt1,
+    description: "This beautiful and iconic T-shirt will no doubt... "
+  },
+  {
+    title: "Chartres Cathedrales",
+    image: ImagesShirt2,
     description: "Lizards are a widespread group of squamate "
   },
   {
-    title: "Bag",
-    image: ImagesBag,
+    title: "Coat of Arms",
+    image: ImagesShirt3,
     description: "Lizards are a widespread group of squamate "
   },
   {
-    title: "Bag",
-    image: ImagesBag,
+    title: "Gallick Cock",
+    image: ImagesShirt4,
     description: "Lizards are a widespread group of squamate "
   },
   {
-    title: "Bag",
-    image: ImagesBag,
+    title: "Marianne",
+    image: ImagesShirt5,
+    description: "Lizards are a widespread group of squamate "
+  },
+  {
+    title: "Alsace",
+    image: ImagesShirt6,
+    description: "Lizards are a widespread group of squamate "
+  },
+  {
+    title: "Apocalypse Tapestry",
+    image: ImagesShirt7,
+    description: "Lizards are a widespread group of squamate "
+  },
+  {
+    title: "Centeur",
+    image: ImagesShirt8,
+    description: "Lizards are a widespread group of squamate "
+  },
+  {
+    title: "T-shirt 9",
+    image: ImagesShirt9,
+    description: "Lizards are a widespread group of squamate "
+  },
+  {
+    title: "T-shirt 10",
+    image: ImagesShirt10,
+    description: "Lizards are a widespread group of squamate "
+  },
+  {
+    title: "T-shirt 11",
+    image: ImagesShirt11,
+    description: "Lizards are a widespread group of squamate "
+  },
+  {
+    title: "T-shirt 12",
+    image: ImagesShirt12,
+    description: "Lizards are a widespread group of squamate "
+  },
+  {
+    title: "T-shirt 13",
+    image: ImagesShirt13,
+    description: "Lizards are a widespread group of squamate "
+  },
+  {
+    title: "T-shirt 14",
+    image: ImagesShirt14,
+    description: "Lizards are a widespread group of squamate "
+  },
+  {
+    title: "T-shirt 15",
+    image: ImagesShirt15,
+    description: "Lizards are a widespread group of squamate "
+  },
+  {
+    title: "T-shirt 16",
+    image: ImagesShirt16,
+    description: "Lizards are a widespread group of squamate "
+  },
+  {
+    title: "T-shirt 17",
+    image: ImagesShirt17,
     description: "Lizards are a widespread group of squamate "
   }
 ];
