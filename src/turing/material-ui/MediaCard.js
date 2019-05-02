6f1e4a5af64cc4a7e528d48a0ada9089f9a13673
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2019-05-02 18:13:25 +0000

    Customized CardMedia

--- src/turing/material-ui/MediaCard.js
+++ src/turing/material-ui/MediaCard.js
@@ -3,16 +3,15 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
+import CardHeader from "@material-ui/core/CardHeader";
 import Typography from "@material-ui/core/Typography";
 import Bag from "../turing-uiux/Zeplin/Images/images-bag.png";
 
 const styles = {
   card: {
-    maxWidth: 345
+    // maxWidth: 345
   },
   media: {
     height: 140
@@ -23,6 +22,7 @@ function MediaCard(props) {
   const { classes } = props;
   return (
     <Card className={classes.card}>
+      <CardHeader title="Bag" />
       <CardActionArea>
         <CardMedia
           className={classes.media}
@@ -30,23 +30,12 @@ function MediaCard(props) {
           title="Contemplative Reptile"
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Lizard
-          </Typography>
           <Typography component="p">
             Lizards are a widespread group of squamate reptiles, with over 6,000
             species, ranging across all continents except Antarctica
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
     </Card>
   );
 }
