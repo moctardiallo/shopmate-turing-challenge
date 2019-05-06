4e451aa5119118aa6fe6dc49ae8f735a6d72945c
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2019-05-06 01:39:55 +0000

    Added buttons to SignIn and Register text

--- src/turing/TopBar.js
+++ src/turing/TopBar.js
@@ -1,21 +1,45 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import {
+  Grid,
+  Button,
+  createMuiTheme,
+  MuiThemeProvider
+} from "@material-ui/core";
+
+const theme = createMuiTheme({
+  typography: {
+    button: {
+      textTransform: "capitalize"
+    }
+  }
+});
 
 function TopBar() {
   return (
-    <Grid container>
-      <Grid item sm={2}>
-        <div style={{ marginLeft: "20px" }}>Hi! SignIn or Register</div>
+    <MuiThemeProvider theme={theme}>
+      <Grid container>
+        <Grid item sm={2}>
+          <div style={{ marginLeft: "20px" }}>
+            Hi!
+            <Button small color="secondary" size="small">
+              SignIn
+            </Button>
+            or
+            <Button small color="secondary" size="small">
+              Register
+            </Button>{" "}
+          </div>
+        </Grid>
+        <Grid item sm={3} />
+        <Grid item sm={3}>
+          Daily Sells - Sell - Help & Contact
+        </Grid>
+        <Grid item sm={2} />
+        <Grid item container justify="flex-end" sm={2}>
+          <div style={{ marginRight: "10px" }}>Your Bag: $0.00</div>
+        </Grid>
       </Grid>
-      <Grid item sm={3} />
-      <Grid item sm={3}>
-        Daily Sells - Sell - Help & Contact
-      </Grid>
-      <Grid item sm={2} />
-      <Grid item container justify="flex-end" sm={2}>
-        <div style={{ marginRight: "10px" }}>Your Bag: $0.00</div>
-      </Grid>
-    </Grid>
+    </MuiThemeProvider>
   );
 }
 
