8c87898825993ad9df59be473aff731424fbfaf1
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2019-05-06 02:33:08 +0000

    Refactor to Register

--- src/turing/material-ui/FormDialog.js
+++ src/turing/material-ui/FormDialog.js
@@ -1,11 +1,6 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
+import Register from "../Register";
 
 function FormDialog(props) {
   const [open, setOpen] = React.useState(false);
@@ -15,44 +10,12 @@ function FormDialog(props) {
     setOpen(true);
   }
 
-  function handleClose() {
-    setOpen(false);
-  }
-
   return (
     <span>
       <Button mini color="secondary" size="small" onClick={handleClickOpen}>
         {title}
       </Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="form-dialog-title"
-      >
-        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here.
-            We will send updates occasionally.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleClose} color="primary">
-            Subscribe
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <Register open={open} setOpen={setOpen} />
     </span>
   );
 }
