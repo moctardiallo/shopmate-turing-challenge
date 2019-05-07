fd1b02ad280c1c7b54355370302a2a2f49302dfc
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2019-05-07 00:40:33 +0000

    Added Register form

--- src/turing/Register.js
+++ src/turing/Register.js
@@ -6,9 +6,38 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import {
+  Grid,
+  withStyles,
+  createMuiTheme,
+  MuiThemeProvider
+} from "@material-ui/core";
 
-function Register() {
+const theme = createMuiTheme({});
+
+const styles = {
+  createAccount: {
+    backgroundColor: theme.palette.secondary.main,
+    color: "#ffffff"
+  },
+  createAccountContainer: {
+    paddingLeft: "0px"
+  },
+
+  login: {
+    color: "#00fbff"
+  },
+  password: {
+    paddingRight: "5px"
+  },
+  confirmPassword: {
+    paddingLeft: "5px"
+  }
+};
+
+function Register(props) {
   const [open, setOpen] = React.useState(false);
+  const { classes } = props;
 
   function handleClickOpen() {
     setOpen(true);
@@ -28,32 +57,70 @@ function Register() {
         onClose={handleClose}
         aria-labelledby="form-dialog-title"
       >
-        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+        <DialogTitle id="form-dialog-title">Register</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here.
-            We will send updates occasionally.
-          </DialogContentText>
+          <DialogContentText>All fields are required*</DialogContentText>
           <TextField
             autoFocus
             margin="dense"
             id="name"
-            label="Email Address"
+            label="Name *"
+            type="text"
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email *"
             type="email"
             fullWidth
           />
+          <Grid container>
+            <Grid item sm={6}>
+              <div className={classes.password}>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="name"
+                  label="Password"
+                  type="password"
+                  fullWidth
+                />
+              </div>
+            </Grid>
+            <Grid item sm={6}>
+              <div className={classes.confirmPassword}>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="name"
+                  label="Confirm your Password *"
+                  type="password"
+                  fullWidth
+                />
+              </div>
+            </Grid>
+          </Grid>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleClose} color="primary">
-            Subscribe
-          </Button>
+          <MuiThemeProvider theme={theme}>
+            <div className={classes.createAccountContainer}>
+              <Button onClick={handleClose} className={classes.createAccount}>
+                create an account
+              </Button>
+            </div>
+          </MuiThemeProvider>
         </DialogActions>
+        <div style={{ padding: "25px" }}>
+          Allready have an account?{" "}
+          <Button size="small" className={classes.login}>
+            Login
+          </Button>
+        </div>
       </Dialog>
     </span>
   );
 }
 
-export default Register;
+export default withStyles(styles)(Register);
