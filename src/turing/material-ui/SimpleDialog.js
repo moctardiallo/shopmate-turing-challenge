08b524c539b0a42ef9622e2515c9d9b8d30ee6db
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2019-05-08 17:35:52 +0000

    Add Item component to SimpleDialog

--- src/turing/material-ui/SimpleDialog.js
+++ src/turing/material-ui/SimpleDialog.js
@@ -1,16 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Avatar from "@material-ui/core/Avatar";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import ListItemText from "@material-ui/core/ListItemText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Dialog from "@material-ui/core/Dialog";
-import PersonIcon from "@material-ui/icons/Person";
-import AddIcon from "@material-ui/icons/Add";
-
-const emails = ["username@gmail.com", "user02@gmail.com"];
+import Item from "../Item";
 
 function SimpleDialog(props) {
   const { onClose, selectedValue, ...other } = props;
@@ -19,43 +11,14 @@ function SimpleDialog(props) {
     onClose(selectedValue);
   }
 
-  function handleListItemClick(value) {
-    onClose(value);
-  }
-
   return (
     <Dialog
       onClose={handleClose}
       aria-labelledby="simple-dialog-title"
       {...other}
     >
-      <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
-      <div>
-        <List>
-          {emails.map(email => (
-            <ListItem
-              button
-              onClick={() => handleListItemClick(email)}
-              key={email}
-            >
-              <ListItemAvatar>
-                <Avatar>
-                  <PersonIcon />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary={email} />
-            </ListItem>
-          ))}
-          <ListItem button onClick={() => handleListItemClick("addAccount")}>
-            <ListItemAvatar>
-              <Avatar>
-                <AddIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary="add account" />
-          </ListItem>
-        </List>
-      </div>
+      <DialogTitle id="simple-dialog-title">Item</DialogTitle>
+      <Item />
     </Dialog>
   );
 }
